afb9517b4b5e60eae8fbe4aba250c20fab50de95
milacerveira <milascerveira@gmail.com>
2021-02-15 18:49:04 +0000

    lists

--- ThemeParkStartPoint/src/main/java/ThemePark.java
+++ ThemeParkStartPoint/src/main/java/ThemePark.java
@@ -1,2 +1,10 @@
+import attractions.Attraction;
+import stalls.Stall;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThemePark {
+    private List<Attraction> attractions = new ArrayList<>();
+    private List<Stall> stalls = new ArrayList<>();
 }
